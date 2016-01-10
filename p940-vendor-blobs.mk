470318865b9351c7656793cc2cdefd3c8ba01fde
animepain <dangvandau2014@gmail.com>
2016-01-10 23:12:06 +0700

    Update p940-vendor-blobs.mk

--- p940-vendor-blobs.mk
+++ p940-vendor-blobs.mk
@@ -1,3 +1,4 @@
+
 # Copyright (C) 2012 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,8 +14,34 @@
 # limitations under the License.
 
 PRODUCT_COPY_FILES += \
-    vendor/lge/p940/proprietary/vendor/firmware/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so \
-    vendor/lge/p940/proprietary/lib/hw/nfc.default.so:system/lib/hw/nfc.default.so \
-    vendor/lge/p940/proprietary/vendor/firmware/ducati-m3_p940.bin:system/vendor/firmware/ducati-m3.bin \
-    vendor/lge/p940/proprietary/lib/hw/sensors.omap4.so:system/lib/hw/sensors.omap4.so \
-    vendor/lge/p940/proprietary/bin/sensord:system/bin/sensord
+    vendor/lge/p940/proprietary/etc/cert/lge.cer:system/etc/cert/lge.cer \
+    vendor/lge/p940/proprietary/etc/firmware/fw_bcmdhd.bin:system/etc/firmware/fw_bcmdhd.bin \
+    vendor/lge/p940/proprietary/etc/firmware/fw_bcmdhd_apsta.bin:system/etc/firmware/fw_bcmdhd_apsta.bin \
+    vendor/lge/p940/proprietary/etc/firmware/fw_bcmdhd_p2p.bin:system/etc/firmware/fw_bcmdhd_p2p.bin \
+    vendor/lge/p940/proprietary/etc/firmware/BCM4330B1_002.001.003.0750.0865.hcd:system/etc/firmware/BCM4330B1_002.001.003.0750.0865.hcd \
+    vendor/lge/p940/proprietary/lib/libril.so:system/lib/libril.so \
+    vendor/lge/p940/proprietary/lib/hw/gps.omap4.so:system/lib/hw/gps.omap4.so \
+    vendor/lge/p940/proprietary/lib/libini.so:system/lib/libini.so \
+    vendor/lge/p940/proprietary/lib/lge-ril.so:system/lib/lge-ril.so \
+    vendor/lge/p940/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+    vendor/lge/p940/proprietary/vendor/lib/libPVRScopeServices_SGX540_120.so:system/vendor/lib/libPVRScopeServices_SGX540_120.so \
+    vendor/lge/p940/proprietary/vendor/lib/liblvverx.so:system/vendor/lib/liblvverx.so \
+    vendor/lge/p940/proprietary/vendor/lib/libIMGegl_SGX540_120.so:system/vendor/lib/libIMGegl_SGX540_120.so \
+    vendor/lge/p940/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+    vendor/lge/p940/proprietary/vendor/lib/libsrv_init_SGX540_120.so:system/vendor/lib/libsrv_init_SGX540_120.so \
+    vendor/lge/p940/proprietary/vendor/lib/libpvrANDROID_WSEGL_SGX540_120.so:system/vendor/lib/libpvrANDROID_WSEGL_SGX540_120.so \
+    vendor/lge/p940/proprietary/vendor/lib/libglslcompiler_SGX540_120.so:system/vendor/lib/libglslcompiler_SGX540_120.so \
+    vendor/lge/p940/proprietary/vendor/lib/hw/gralloc.omap4430.so:system/vendor/lib/hw/gralloc.omap4430.so \
+    vendor/lge/p940/proprietary/vendor/lib/libpvr2d_SGX540_120.so:system/vendor/lib/libpvr2d_SGX540_120.so \
+    vendor/lge/p940/proprietary/vendor/lib/liblvvetx.so:system/vendor/lib/liblvvetx.so \
+    vendor/lge/p940/proprietary/vendor/lib/libsmapi.so:system/vendor/lib/libsmapi.so \
+    vendor/lge/p940/proprietary/vendor/lib/libsrv_um_SGX540_120.so:system/vendor/lib/libsrv_um_SGX540_120.so \
+    vendor/lge/p940/proprietary/vendor/lib/libusc_SGX540_120.so:system/vendor/lib/libusc_SGX540_120.so \
+    vendor/lge/p940/proprietary/vendor/lib/libtf_crypto_sst.so:system/vendor/lib/libtf_crypto_sst.so \
+    vendor/lge/p940/proprietary/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so \
+    vendor/lge/p940/proprietary/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so:system/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so \
+    vendor/lge/p940/proprietary/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
+    vendor/lge/p940/proprietary/vendor/bin/pvrsrvctl_SGX540_120:system/vendor/bin/pvrsrvctl_SGX540_120 \
+    vendor/lge/p940/proprietary/vendor/bin/pvrsrvinit:system/vendor/bin/pvrsrvinit \
+    vendor/lge/p940/proprietary/bin/glgps:system/bin/glgps \
+    vendor/lge/p940/proprietary/bin/rild:system/bin/rild
